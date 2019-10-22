1fda2647f9cee4dca6a4bb40e7156e80ed5c5981
lgiacome <lorenzo.giacomel@cern.ch>
2019-10-22 15:38:42 -0700

    Introduced kwargs

--- script_measure_SEY_single.py
+++ script_measure_SEY_single.py
@@ -1,10 +1,10 @@
 import numpy as np
 from  measure_SEY_pyecloud import measure_SEY
 
-run_in_other_process = False
+run_in_other_process = True
 enable_trap = False
 
-ene = 0.003
+ene = 5.
 
 from run_in_separate_process import run_in_separate_process
 import sys
@@ -23,15 +23,19 @@ sey_params_dict['sigmafit'] = 1.09
 sey_params_dict['mufit'] = 1.66
 sey_params_dict['secondary_angle_distribution'] = 'cosine_3D'
 
-
+kwargs = {
+        'Ekin': ene, 
+        'Nmp': Nmp,
+        'N_elec_p_mp': N_elec_p_mp, 
+        'sey_params_dict': sey_params_dict,
+        'flag_video':False 
+    }
 sys.stdout = text_trap
 
 if run_in_other_process:
-    res = run_in_separate_process(measure_SEY, [ene, Nmp, N_elec_p_mp,
-        sey_params_dict, True])
+    res = run_in_separate_process(measure_SEY, kwargs=kwargs)
 else:
-    res = measure_SEY(ene, Nmp, N_elec_p_mp,
-        sey_params_dict, True)
+    res = measure_SEY(**kwargs)
 
 sey_curve = res['SEY']
 sys.stdout = original
@@ -45,4 +49,10 @@ del_ref, _ = yield_fun2(E=np.array([ene]), costheta=1., s=1.35, E0=150,
     R0=sey_params_dict['R0'])
 
 
-
+import matplotlib.pyplot as plt
+plt.close('all')
+fig1 = plt.figure(1)
+sp1 = fig1.add_subplot(111)
+sp1.plot(res['n_secelecs'], '.-')
+sp1.plot(res['n_primelecs'], '.-')
+plt.show()
