8a4473e1d9a1b0f5ccabd74abe276ebf28d8ab58
Lorenzo Giacomel <lorenzo.giacomel@cern.ch>
2019-10-30 18:56:56 +0100

    modified soft regeneration to act without restart

--- perform_regeneration.py
+++ perform_regeneration.py
@@ -2,53 +2,61 @@ import numpy as np
 from numpy.random import rand
 from warp import echarge as charge
 from warp import emass as mass
+from warp import top
+#from warp.particles.Secondaries import Secondaries
 
-def perform_regeneration(target_N_mp, x_mp, y_mp, z_mp, vx_mp, vy_mp, vz_mp, nel_mp):
-    N_mp = len(x_mp)
+def perform_regeneration(target_N_mp, wsp, Sec):
+    # Just defining a shortcut
+    # Old number of MPs
+    N_mp = wsp.getn()
+    old_N_mp = N_mp
     if N_mp > target_N_mp:
-        chrg = np.sum(nel_mp)
-        erg = np.sum(0.5 / np.abs(charge / mass) * nel_mp[0:N_mp] * (vx_mp[0:N_mp] * vx_mp[0:N_mp] +  vy_mp[0:N_mp] * vy_mp[0:N_mp] + vz_mp[0:N_mp] * vz_mp[0:N_mp]))
+	# Compute old total charge and Ekin
+        chrg = np.sum(wsp.getw())
+        erg = np.sum(0.5 / np.abs(charge / mass) * wsp.getw()[0:N_mp] * (wsp.getvx()[0:N_mp] * wsp.getvx()[0:N_mp] + wsp.getvy()[0:N_mp] * wsp.getvy()[0:N_mp] + wsp.getvz()[0:N_mp] * wsp.getvz()[0:N_mp]))
         
-        new_nel_mp_ref = chrg / target_N_mp
-                    
+
+        new_nel_mp_ref = chrg / target_N_mp  
+        Sec.set_nel_mp_ref(new_nel_mp_ref)
+	print 'New nel_mp_ref = %d'%(Sec.pyecloud_nel_mp_ref)
+
         print 'Start SOFT regeneration. N_mp=%d Nel_tot=%1.2e En_tot=%1.2e'%(N_mp, chrg, erg)
                     
-        #set_nel_mp_ref(new_nel_mp_ref)
-                    
-        death_prob = float(N_mp - target_N_mp) / float(N_mp)
-                    
-        flag_keep = np.array(len(x_mp) * [False])
-        flag_keep[:N_mp] = (rand(N_mp) > death_prob)
-        N_mp = np.sum(flag_keep)
 
-	x_mp_new = np.zeros(N_mp)
-	y_mp_new = np.zeros(N_mp)
-	z_mp_new = np.zeros(N_mp)
-	vx_mp_new = np.zeros(N_mp)
-	vy_mp_new = np.zeros(N_mp)
-	vz_mp_new = np.zeros(N_mp)
-	nel_mp_new = np.zeros(N_mp)
-                  
-        x_mp_new[0:N_mp] = np.array(x_mp[flag_keep].copy())
-        y_mp_new[0:N_mp] = np.array(y_mp[flag_keep].copy())
-        z_mp_new[0:N_mp] = np.array(z_mp[flag_keep].copy())
-        vx_mp_new[0:N_mp] = np.array(vx_mp[flag_keep].copy())
-        vy_mp_new[0:N_mp] = np.array(vy_mp[flag_keep].copy())
-        vz_mp_new[0:N_mp] = np.array(vz_mp[flag_keep].copy())
-        nel_mp_new[0:N_mp] = np.array(nel_mp[flag_keep].copy())
-                    
-           
-        chrg_before = chrg
-        chrg_after = np.sum(nel_mp_new)
-                    
+	# Compute the death probability         
+        death_prob = float(N_mp - target_N_mp) / float(N_mp)
+	# Decide which MPs to keep/discard
+        flag_keep = np.array(N_mp * [False])
+        flag_keep[:N_mp] = (rand(N_mp) > death_prob)
+	           
+        # Compute the correction factor	
+	chrg_before = chrg
+        chrg_after = np.sum(wsp.getw()[flag_keep])
         correct_fact = chrg_before / chrg_after
-                    
+
+	# Compute the indices of the MPs in pgroup.pid
+        i_init = top.pgroup.ins[wsp.getjs()] - 1 
+	inds_clear = np.where(flag_keep == False)[0] + i_init
+	inds_keep = np.where(flag_keep == True)[0] + i_init
+
+	# Rescale the survivors
+	for ii in inds_keep:
+		# Not clear why I am using wpid-1, but it seems to work 
+       		top.pgroup.pid[ii,top.wpid-1] = top.pgroup.pid[ii,top.wpid-1]*correct_fact
+	
+	# Kill the discarded MPs
+	for ii in inds_clear:
+                top.pgroup.gaminv[ii] = 0.
+
+	# Warning: top.clearpart is a Fortran routine so it has one-based ordering..
+	top.clearpart(top.pgroup, wsp.getjs()+1, 1)        
+	# Compute new number of MPs
+	N_mp = wsp.getn()
+
         print 'Applied correction factor = %e'%correct_fact
                    
-        nel_mp_new[0:N_mp] = nel_mp_new[0:N_mp] * correct_fact
-                    
-        chrg = np.sum(nel_mp_new)
-        erg = np.sum(0.5 / np.abs(charge / mass) * nel_mp_new[0:N_mp] * (vx_mp_new[0:N_mp] * vx_mp_new[0:N_mp] + vy_mp_new[0:N_mp] * vy_mp_new[0:N_mp] + vz_mp_new[0:N_mp] * vz_mp_new[0:N_mp]))
-                
+	                    
+        chrg = np.sum(wsp.getw())
+        erg = np.sum(0.5 / np.abs(charge / mass) * wsp.getw()[0:N_mp] * (wsp.getvx()[0:N_mp] * wsp.getvx()[0:N_mp] + wsp.getvy()[0:N_mp] * wsp.getvy()[0:N_mp] + wsp.getvz()[0:N_mp] * wsp.getvz()[0:N_mp]))        
         print 'Done SOFT regeneration. N_mp=%d Nel_tot=%1.2e En_tot=%1.2e'%(N_mp, chrg, erg)
-    return x_mp_new,y_mp_new,z_mp_new,vx_mp_new,vy_mp_new,vz_mp_new,nel_mp_new,N_mp
+
